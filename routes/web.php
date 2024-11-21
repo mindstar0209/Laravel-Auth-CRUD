1f9c86fd7df2cfca408472cf0718a876c793c567
mindstar0209 <best.mindstar@gmail.com>
2024-11-22 00:57:30 +0900

    push my code first

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\NoteController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    $title = "Dashboard";
+    return view('dashboard', compact("title"));
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::resource("notes", NoteController::class)->middleware(['auth', 'verified']);
+
+require __DIR__ . '/auth.php';
